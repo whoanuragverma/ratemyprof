a18fff13c2a56cb652d838fb72f10466f19dbf1e
Anurag Verma <anu.anuragverma@gmail.com>
2020-06-10 10:26:29 +0530

    Completed -- ADS left

--- client/src/components/profreview.js
+++ client/src/components/profreview.js
@@ -1,10 +1,23 @@
 import React, { useEffect, useState } from "react";
-import { List, Avatar, Skeleton } from "antd";
+import {
+    List,
+    Avatar,
+    Skeleton,
+    Divider,
+    Form,
+    Input,
+    Switch,
+    Button,
+} from "antd";
+import { ArrowRightOutlined } from "@ant-design/icons";
 
+const { TextArea } = Input;
 const ProfReview = (props) => {
     const [res, setRes] = useState([]);
     const [Name, setName] = useState("");
     const [loading, setLoading] = useState(true);
+    const [checked, setChecked] = useState(false);
+    const [hasGivenReview, sethasGivenReview] = useState(false);
     const view = () => {
         fetch("/api/read_review", {
             method: "POST",
@@ -48,6 +61,9 @@ const ProfReview = (props) => {
                 "#" + Math.floor(Math.random() * 16777215).toString(16),
         };
     };
+    const handleClick = () => {
+        setChecked(!checked);
+    };
     const toNormalDate = (d) => {
         const monthNames = [
             "January",
@@ -71,6 +87,99 @@ const ProfReview = (props) => {
             ", " +
             date.getFullYear()
         );
+    };
+    const sendReview = (values) => {
+        values.anonymous = checked;
+        fetch("/api/write_review", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "x-access-token": localStorage.getItem("token"),
+            },
+            body: JSON.stringify({
+                anonymous: values.anonymous,
+                review: values.review,
+                FID: props.FID,
+            }),
+        })
+            .then((res) => res.json())
+            .then((res) => {
+                console.log(res);
+                view();
+            });
+    };
+    const giveUserComments = () => {
+        fetch("/api/check_review", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "x-access-token": localStorage.getItem("token"),
+            },
+            body: JSON.stringify({ FID: props.FID }),
+        })
+            .then((res) => res.json())
+            .then((res) => sethasGivenReview(res.response));
+        if (!hasGivenReview) {
+            return (
+                <React.Fragment>
+                    <Divider style={{ margin: "10px 0 10px 0" }} />
+                    <center>
+                        We limit the number of certain actions you perform on
+                        this site to prevent <b>spam</b>.<br />
+                        Looks like you have already written something. Currently
+                        we don't allow to edit reviews.
+                    </center>
+                </React.Fragment>
+            );
+        } else {
+            return (
+                <React.Fragment>
+                    <Divider style={{ margin: "10px 0 10px 0" }} />
+                    <Form name="userReview" onFinish={sendReview}>
+                        <Form.Item name="anonymous" valuePropName="checked">
+                            <Switch
+                                unCheckedChildren={
+                                    <React.Fragment>Yourself 😎</React.Fragment>
+                                }
+                                checkedChildren={
+                                    <React.Fragment>
+                                        Anonymous 🕵🏻‍♂️
+                                    </React.Fragment>
+                                }
+                                onClick={handleClick}
+                            />
+                        </Form.Item>
+                        <Form.Item
+                            name="review"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: "Write something first!",
+                                },
+                            ]}
+                        >
+                            <TextArea
+                                placeholder="Write something here. Once you post the review, you cannot edit it. "
+                                autoSize={{ minRows: 3, maxRows: 10 }}
+                            />
+                        </Form.Item>
+                        <Form.Item>
+                            <Button
+                                type="dashed"
+                                htmlType="submit"
+                                style={{
+                                    position: "relative",
+                                    left: "100%",
+                                    transform: "translateX(-100%)",
+                                }}
+                            >
+                                Post {<ArrowRightOutlined />}
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                </React.Fragment>
+            );
+        }
     };
     const UserComments = () => {
         if (res.length === 0) {
@@ -143,6 +252,7 @@ const ProfReview = (props) => {
                 }}
             >
                 <Skeleton loading={loading}>{UserComments()}</Skeleton>
+                <Skeleton loading={loading}>{giveUserComments()}</Skeleton>
             </div>
         </React.Fragment>
     );
